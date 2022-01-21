c01ce309c57e62cda50234319616783dd6ba44dc
maritaalva <malva3239@gmail.com>
2022-01-21 10:22:12 +0100

    bartulos_2

--- app/main.js
+++ app/main.js
@@ -1,31 +1,38 @@
 /** Cargar un fichero JSON */
-window.onload = () => {
-    let lista = document.querySelector("#usuarios");
-    let detalle = document.querySelector(".detail");
-    let datos;
+// window.onload = () => {
+//     let lista = document.querySelector("#usuarios");
+//     let detalle = document.querySelector(".detail");
+//     let datos;
 
-    let mostrarDetalle = (e) =>{
-        window.open(`../detalle.html?id=${e.currentTarget.id}`,'_blank');
-    }
+//     let mostrarDetalle = (e) =>{
+//         window.open(`../detalle.html?id=${e.currentTarget.id}`,'_blank');
+//     }
 
-    fetch('assets/data/data.json')
-    .then(res => res.json())
-    .then(data => {
-        datos = data;
-       data.forEach((user, index) => {
-           let item=`<li class="user" id="${index}">
-                    <img src="${user.image}" alt="${user.first_name} "/>
-                    <span>${user.first_name} ${user.last_name}</span>
-                </li>`;
-         lista.innerHTML += item;
-       });
-     })
-     .then(()=>{
-        let users = document.querySelectorAll(".user");
-        users.forEach((user) => {
-            user.addEventListener("click", mostrarDetalle, true)
-        })
-     });
+//     fetch('assets/data/data.json')
+//     .then(res => res.json())
+//     .then(data => {
+//         datos = data;
+//        data.forEach((user, index) => {
+//            let item=`<li class="user" id="${index}">
+//                     <img src="${user.image}" alt="${user.first_name} "/>
+//                     <span>${user.first_name} ${user.last_name}</span>
+//                 </li>`;
+//          lista.innerHTML += item;
+//        });
+//      })
+//      .then(()=>{
+//         let users = document.querySelectorAll(".user");
+//         users.forEach((user) => {
+//             user.addEventListener("click", mostrarDetalle, true)
+//         })
+//      });
+let ancho = window.innerWidth;
+let roww = ancho/14;
+window.onload = (e) => {
+
+    document.getElementById("grid").style.gridTemplateRows = "repeat (12, 1fr)";
+ console.log (ancho)
+ console.log(roww)
+
 
 }
-
