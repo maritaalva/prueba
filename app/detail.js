77b996e11e5721bf010be2e36adac571b3f3c272
maritaalva <malva3239@gmail.com>
2022-01-21 16:14:25 +0100

    bartulos_5

--- app/detail.js
+++ app/detail.js
@@ -11,6 +11,11 @@ window.onload = () => {
         .then(res => res.json())
         .then(data => {
             let user = data[Number(param)]
-            document.write(user.first_name)
-    })
+            document.write(user.title)
+            // let text_box = documentquerySelector(".text_box");
+            // let main = `<h2> ${user.title} </h2> <img class="img_art_01_1" src"${user.image1_1}" alt="african_chair"><img class="img_ess_01_1" src"${user.image2_1}" alt="ilustration"><img class="img_art_02_1" src"${user.image2_1}" alt="monobloc_chair">`
+            // let content = `<p class="contenido">${user.content}</p>`;
+            // main += content;
+            // text_box.innerHTML = main
+        })
 }
